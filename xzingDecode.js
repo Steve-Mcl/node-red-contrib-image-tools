a9cef56f689979475db7955ceda9af3500f432c6
Steve-Mcl <sdmclaughlin@gmail.com>
2020-04-11 18:19:43 +0100

    Add UI options to barcode decode node edit form

--- xzingDecode.js
+++ xzingDecode.js
@@ -8,66 +8,78 @@ module.exports = function(RED) {
 
         var node = this;
 		node.data = config.data || "payload";//data
-		node.dataType = config.dataType || "msg";
-        node.specification = config.specification || '{"QR": false, "dataMatrix": false, "tryHarder": true}';//specification
-        node.specificationType = config.specificationType || "json";
+        node.dataType = config.dataType || "msg";
+        
+        node.tryHarder = config.tryHarder || false;
+        node.tryHarderType = config.tryHarderType || "bool";
 
-          function decode2d(img,options){
-           
-            const bitmap = img.bitmap;
-            const hints = new Map();
-            const formats = [];
-            const defaults = {format : ["QR", "DATA_MATRIX"], tryHarder: true};
-            options = options || {};
-            options.QR = options.QR == undefined ? defaults.QR : options.QR;
-            options.dataMatrix = options.dataMatrix == undefined ? defaults.dataMatrix : options.dataMatrix;
-            options.tryHarder = options.tryHarder == undefined ? defaults.tryHarder : options.tryHarder;
-            
-            
-            if(options.format){
-                let _formats = options.format;
-                if(Array.isArray(_formats) === false){
-                    _formats = [_formats];
-                }
-                for (let index = 0; index < _formats.length; index++) {
-                    const format = BarcodeFormat[_formats[index]];
-                    if(format || format === 0){
-                        if(formats.includes(format) === false){
-                            formats.push(format);
+        node.DATA_MATRIX = config.DATA_MATRIX;
+        node.QR_CODE = config.QR_CODE;
+        node.PDF_417 = config.PDF_417;
+        node.EAN_8 = config.EAN_8;
+        node.EAN_13 = config.EAN_13;
+        node.CODE_39 = config.CODE_39;
+        node.CODE_128 = config.CODE_128;
+        node.ITF = config.ITF;
+        node.RSS_14 = config.RSS_14;
+    debugger;
+        node.formats = [];
+        let stringformats = []
+        if(node.DATA_MATRIX) {stringformats.push("DATA_MATRIX")}
+        if(node.QR_CODE) {stringformats.push("QR_CODE")}
+        if(node.PDF_417) {stringformats.push("PDF_417")}
+        if(node.EAN_8) {stringformats.push("EAN_8")}
+        if(node.EAN_13) {stringformats.push("EAN_13")}
+        if(node.CODE_39) {stringformats.push("CODE_39")}
+        if(node.CODE_128) {stringformats.push("CODE_128")}
+        if(node.ITF) {stringformats.push("ITF")}
+        if(node.RSS_14) {stringformats.push("RSS_14")}
+        node.formats = barcodeStringFormatsConvertor(stringformats);
+
+        function barcodeStringFormatsConvertor(ArrayOfStringFormats){
+            let formats = null;
+            try {
+                if(ArrayOfStringFormats && ArrayOfStringFormats.length){
+                    formats = [];
+                    for (let index = 0; index < ArrayOfStringFormats.length; index++) {
+                        const format = BarcodeFormat[ArrayOfStringFormats[index]];
+                        if(format || format === 0){
+                            if(formats.includes(format) === false){
+                                formats.push(format);
+                            }
                         }
                     }
-                }
-            }
-         
-            //legacy options - user should use options.format
-            if(options.QR && formats.includes(BarcodeFormat.QR_CODE) === false)
-                formats.push(BarcodeFormat.QR_CODE);
-            if(options.dataMatrix && formats.includes(BarcodeFormat.DATA_MATRIX) === false)
-                formats.push(BarcodeFormat.DATA_MATRIX);
+                } 
+            } catch (error) {
+            }            
+            return formats;
+        }
 
-            if(formats.length)
-                hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
-            if(options.tryHarder)
-                hints.set(DecodeHintType.TRY_HARDER, true);
-          
+        function decodeBarcode(img, formats, tryHarder){
+            if(!img || !img.bitmap){
+                throw new Error("Image is not valid. Expected an object with a bitmap property.")
+            }
+            const bitmap = img.bitmap;
+            const hints = new Map();
             const reader = new MultiFormatReader();
-            reader.setHints(hints);
-          
             const len = bitmap.width * bitmap.height;
             const luminancesUint8Array = new Uint8Array(len);
-          
+
+            if(formats && Array.isArray(formats) && formats.length)
+                hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
+                
+            if(tryHarder)
+                hints.set(DecodeHintType.TRY_HARDER, true);
+        
+            reader.setHints(hints);
             for(let i = 0; i < len; i++){
-              luminancesUint8Array[i] = ((bitmap.data[i*4]+bitmap.data[(i*4)+1]+bitmap.data[(i*4)+2]) / 4) & 0xFF;
+            luminancesUint8Array[i] = ((bitmap.data[i*4]+bitmap.data[(i*4)+1]+bitmap.data[(i*4)+2]) / 4) & 0xFF;
             }
-    
             const luminanceSource = new RGBLuminanceSource(luminancesUint8Array, bitmap.width, bitmap.height);
             const binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
             const decoded = reader.decode(binaryBitmap,hints);
-            
             return decoded;
-          }
-
-
+        }
         function isEmpty(obj) {
             for(var key in obj) {
                 if(obj.hasOwnProperty(key))
@@ -98,7 +110,6 @@ module.exports = function(RED) {
 
             node.status({});//clear status
             var data;
-
             RED.util.evaluateNodeProperty(node.data,node.dataType,node,msg,(err,value) => {
                 if (err) {
                     node.error("Unable to evaluate data",msg);
@@ -108,28 +119,30 @@ module.exports = function(RED) {
                     data = value; 
                 }
             }); 
-            var specification;
-            RED.util.evaluateNodeProperty(node.specification,node.specificationType,node,msg,(err,value) => {
+            var tryHarder = false;
+            RED.util.evaluateNodeProperty(node.tryHarder,node.tryHarderType,node,msg,(err,value) => {
                 if (err) {
-                    node.error("Unable to evaluate specification",msg);
-                    node.status({fill:"red",shape:"ring",text:"Unable to evaluate specification"});
+                    node.error("Unable to evaluate tryHarder",msg);
+                    node.status({fill:"red",shape:"ring",text:"Unable to evaluate tryHarder"});
                     return;//halt flow!
                 } else {
-                    specification = value;
+                    tryHarder = value;
                 }
             }); 
             msg.originalPayload = msg.payload;//store original Payload incase user still wants it
+            let formats = null;
+
             try {
                 node.msgMem = msg;
-
-                //check specification - is it a JSON string? convert to object if it is.
-                if(specification && isJSON(specification)){
-                    specification = JSON.parse(specification);
+                if(msg.barcodeFormats){
+                    formats = barcodeStringFormatsConvertor(msg.barcodeFormats);
+                } else {
+                    formats = node.formats;
                 }
-
-                function decode(img,specification){
+                                               
+                function _decode(img, formats, tryHarder){
                     performance.start("decode");
-                    let decoded = decode2d(img,specification);  
+                    let decoded = decodeBarcode(img, formats, tryHarder);  
                     performance.end("decode");
                     performance.end("total");
                     node.msgMem.performance = performance.getPerformance();
@@ -145,13 +158,13 @@ module.exports = function(RED) {
                     performance.end("base64_to_buffer");
                 } 
                 if(data instanceof Jimp){
-                    decode(data,specification);
+                    _decode(data, formats, tryHarder);
                 } else {  
                     performance.start("jimp_read");
                     Jimp.read(data)
                         .then(img => {
                             performance.end("jimp_read");
-                            decode(img,specification);
+                            _decode(img, formats, tryHarder);
                         })
                         .catch(err => {
                             console.error(err);
@@ -163,6 +176,12 @@ module.exports = function(RED) {
                
             } catch (error) {
                 console.error(error);
+                if(error.name = "NotFoundException" || error.message == "No MultiFormat Readers were able to detect the code."){
+                    //do nothing 
+                    node.status({fill:"yellow",shape:"dot",text:"Not found"});
+                    return;//halt flow
+                } 
+                //other errors
                 node.error(error,msg);
                 node.status({fill:"red",shape:"dot",text:"Error"});
                 return;//halt flow
